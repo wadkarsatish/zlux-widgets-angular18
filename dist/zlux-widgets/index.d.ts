305ba016604d907c479870aa79b6425fdfdcc907
Satish Wadkar <swadkar@rocketsoftware.com>
2024-10-09 19:27:34 +0530

    change package name to original one
    Signed-off-by: Satish Wadkar <swadkar@rocketsoftware.com>

--- dist/zlux-widgets/index.d.ts
+++ dist/zlux-widgets/index.d.ts
@@ -1,6 +1,6 @@
 /**
  * Generated bundle index. Do not edit.
  */
-/// <amd-module name="zlux-widgets" />
+/// <amd-module name="@zlux/widgets" />
 export * from './public-api';
 //# sourceMappingURL=zlux-widgets.d.ts.map
